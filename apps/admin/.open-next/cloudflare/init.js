7b3cc3afacb06a25b6be1da86254bb88c5ba43ac
unknown <profinfo101@gmail.com>
2026-09-12 10:39:31 +0100

    Add GitHub Actions for Cloudflare deployment

--- apps/admin/.open-next/cloudflare/init.js
+++ apps/admin/.open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1789203612348,
+    __BUILD_TIMESTAMP_MS__: 1789204569318,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
